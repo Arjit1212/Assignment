6855b1fed9756d89c8b97c75ca99fc3eee5d560d
Arjit raj <rajarjit410@gmail.com.com>
2024-04-09 16:10:45 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react'
 import './App.css';
 import CountDowmTimer from './CountDownTimer';
 import UserList from './UserList';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {HashRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
         <Route exact path='/' element={<CountDowmTimer/>}></Route>
           <Route exact path='/user' element={<UserList/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
